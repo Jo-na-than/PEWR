38d31d55ba4e03f74ad7e1f90ddd4dd44ec0c803
Jo-na-than <jonathan.palacios@orange.fr>
2024-05-06 21:57:21 +0200

    nouvelle presentation page d'accueil

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Fonction pour afficher la date et l'heure actuelles
 
 function afficherDateHeure() {
-  const date = new Date();
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  };
-  const dateHeure = date.toLocaleDateString("fr-FR", options);
-  document.getElementById("dateHeure").innerHTML = dateHeure;
+	const date = new Date();
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric',
+	};
+	const dateHeure = date.toLocaleDateString('fr-FR', options);
+	document.getElementById('dateHeure').innerHTML = dateHeure;
 }
 
 // Appel de la fonction pour afficher la date et l'heure
@@ -22,9 +22,24 @@ afficherDateHeure();
 // Fonction pour afficher un message d'alerte
 
 function afficherAlerte() {
-  alert("Ceci est un message d'alerte !");
+	alert("Ceci est un message d'alerte !");
 }
 
 // Ajout d'un événement click sur le bouton
 
-document.getElementById("boutonAlerte").addEventListener("click", afficherAlerte);
+document
+	.getElementById('boutonAlerte')
+	.addEventListener('click', afficherAlerte);
+
+<script type="text/javascript">
+	$(document).ready(function()
+	{$('.partners-carousel').slick({
+		slidesToShow: 3, // Number of images visible at once
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: true,
+	})}
+	);
+</script>;
